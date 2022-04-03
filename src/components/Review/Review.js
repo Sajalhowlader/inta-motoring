e58f8b56cfcdaa1bc26e5bd250411bb942ffa7cc
Sajalhowlader <sajolhowlader08@gmail.com>
2022-04-03 20:52:36 +0600

    add navbar link name

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Home = () => {
+const Review = () => {
     return (
         <div>
-            <h1>Home Page</h1>
+            <h1>Review Page</h1>
         </div>
     );
 };
 
-export default Home;
+export default Review;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOMClint from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOMClint.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>
 
 );
 
