8447938db0d707da4d561b34bb1335270f9a6676
Sajalhowlader <sajolhowlader08@gmail.com>
2022-04-03 23:46:13 +0600

    add custom hooks

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,17 @@
 import React from 'react';
-
+import carImg from '../../images/car.png'
+import './Home.css'
 const Home = () => {
     return (
-        <div>
-            <h1>Home Page</h1>
+        <div className='product-container'>
+            <div className="product-info">
+                <p className='product-title'>Make Your day < br /><span>with Insta</span></p>
+                <p>Welcome to Insta Motoring Family.Here you can buy New and Recondition Cars.We will provide you 12 month free servicing after buy a cur. Thank You</p>
+                <button className='info-btn'>Explore More</button>
+            </div>
+            <div className='product-img'>
+                <img src={carImg} alt="" />
+            </div>
         </div>
     );
 };
