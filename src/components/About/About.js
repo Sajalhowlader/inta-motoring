392d1880a9e10fb646f34344ae77bd2352fb0421
Sajalhowlader <sajolhowlader08@gmail.com>
2022-04-04 19:47:06 +0600

    add about component

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,9 +1,23 @@
 import React from 'react';
-
+import img1 from '../../images/img-1.jpg'
+import img2 from '../../images/img-2.jpg'
+import img3 from '../../images/img-3.jpg'
+import img4 from '../../images/img-4.jpg'
+import img5 from '../../images/img-5.jpg'
+import img6 from '../../images/img-6.jpg'
+import './About.css'
 const About = () => {
     return (
         <div>
-            <h1>About Page</h1>
+            <h1 className='img-title'>Boos Photo</h1>
+            <div className='img-container'>
+                <img src={img1} alt="" />
+                <img src={img2} alt="" />
+                <img src={img3} alt="" />
+                <img src={img4} alt="" />
+                <img src={img5} alt="" />
+                <img src={img6} alt="" />
+            </div>
         </div>
     );
 };
--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -1,10 +1,45 @@
 import React from 'react';
-
+import './Blogs.css'
 const Blogs = () => {
     return (
-        <div>
-            <h1>Blogs Page</h1>
-        </div>
+        <section className='api-container'>
+            <article className='api-info'>
+                <h1>Contex API</h1>
+                <p>The Context API is a React structure that enables you to exchange unique details and assists in solving prop-drilling from all levels of your application. When we create a common function and pass the function by props to other functions it's pretty boring. We need to write the same thing over and over again. Context API helps us a lot here. We can easily make that function pass in any other component by creating a function in the parent component. It is often the case that we need to send a function between the parent component to the child component and another child component. In this case, if we repeatedly send props, the duplication of our code increases a lot. We create a context between the parent components, whatever the code we send to the child component is DRY and there is no duplication of the code.</p>
+            </article>
+            <article className='api-info'>
+                <h1>Semantic Tag</h1>
+                <p>
+                    Semantic element = elements with a meaning.
+                    A semantic element clearly describes its meaning to both the browser and the developer.
+                    Example of Semantic tag.
+                    1.from <br />
+                    02.table <br />
+                    03.aside <br />
+                    04.header <br />
+                    05. footer etc. <br />
+                    Semantic Tech Implementing Semantic Tech is important for website ranking to help the semantic tag browser robot crawl.With the help of semantic element tags we can easily understand which element has been used for which purpose.Semantic element tags are very important for browser robots and developers because we developers do not easily understand why a tag has been used.
+                </p>
+            </article>
+            <article className='api-info'>
+                <h1>Inline vs block vs inline-block   </h1>
+                <p>
+                    01.Inline:Inline as the name says “included as a part of the main text and not as a separate section”. Inline elements occupy the space as needed within the space defined by the main element. Unlike block-level elements, they do not begin on new lines.
+
+                    Some of the inline elements are a,span,img,code,cite,button,input etc. <br />
+                    <br />
+                    02.Block: A Block-level element occupies the entire space of the parent (container) such as div and p in the example .
+
+                    Note that both div and p start from a new line each time, forming a block-like structure. Block-level elements begin on new lines.
+
+                    Common block-level elements are div,p,article,section,figure,footer etc. <br />
+                    <br />
+                    03. Inline-block:Inline-block elements are similar to inline elements, except they can have padding and margins added on all four sides. You'll have to declare display: inline-block in your CSS code.
+                    One common use for using inline-block is for creating navigation links horizontally, as shown below.
+                </p>
+            </article>
+
+        </section>
     );
 };
 
